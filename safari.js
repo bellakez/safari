cdff4b9b4d248a335756eea8ef7654f04ec14a03
Amanda Hjelgaard <amanda.hjelgaard@hotmail.com>
2022-02-18 14:45:55 +0100

    Ændringer

--- safari.js
+++ safari.js
@@ -64,3 +64,44 @@ function vis() {
     }
   });
 }
+
+// BURGER MENU
+
+document.querySelector("#open").addEventListener("click", openMenu);
+document.querySelector("#close").addEventListener("click", closeMenu);
+
+function openMenu() {
+  document.querySelector(".menu").style.transform = "translateY(0)";
+  document.querySelector("#open").classList.add("hide");
+  document.querySelector("#close").classList.remove("hide");
+}
+
+function closeMenu() {
+  document.querySelector(".menu").style.transform = "translateY(-100%)";
+  document.querySelector("#open").classList.remove("hide");
+  document.querySelector("#close").classList.add("hide");
+}
+
+// BACK TO TOP
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
