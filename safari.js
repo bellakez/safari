aaa21d944311f1f3fb82a1dec0bca5a0d5332d06
Alberte <albe304d@stud.kea.dk>
2022-02-23 17:04:06 +0100

    Burgermenu fjernet

--- safari.js
+++ safari.js
@@ -64,23 +64,6 @@ function vis() {
   });
 }
 
-// BURGER MENU
-
-document.querySelector("#open").addEventListener("click", openMenu);
-document.querySelector("#close").addEventListener("click", closeMenu);
-
-function openMenu() {
-  document.querySelector(".menu").style.transform = "translateY(0)";
-  document.querySelector("#open").classList.add("hide");
-  document.querySelector("#close").classList.remove("hide");
-}
-
-function closeMenu() {
-  document.querySelector(".menu").style.transform = "translateY(-100%)";
-  document.querySelector("#open").classList.remove("hide");
-  document.querySelector("#close").classList.add("hide");
-}
-
 // BACK TO TOP
 
 //Get the button:
