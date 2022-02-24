dd645d036edc0efb597e792867eac96accc01f35
bellakez <bellakezia28@stud.kea.dk>
2022-02-24 13:40:30 +0100

    kommentare css, html, js

--- safari.js
+++ safari.js
@@ -69,7 +69,7 @@ function vis() {
 //Get the button:
 mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+//Når brugeren scroller 20px ned fra top af dokumentet, viser knappen (button) sig
 window.onscroll = function () {
   scrollFunction();
 };
@@ -82,7 +82,7 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+//Når brugeren klikker på knappen (button), scroller den til toppen af dokumentet
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
